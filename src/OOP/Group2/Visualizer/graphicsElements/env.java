b4f24651f486c79be026486cd9938d01a9261996
Duong <2901nguyendinhduong@gmail.com>
2024-06-07 15:08:31 +0700

    Add: env file

---
+++ src/OOP/Group2/Visualizer/graphicsElements/env.java
@@ -0,0 +1,14 @@
+package OOP.Group2.Visualizer.graphicsElements;
+//change your directory path
+public class env {
+        public static String BUTTON_PATH = "%s/src/OOP/Group2/Visualizer/graphicsElements/button/buttonImage/%s.png";
+        public static String MENU_BACKGROUND_PATH = "D:\\github\\SortingDemonstrate\\src\\OOP\\Group2\\Visualizer\\graphicsElements\\background\\menuBackground.gif";
+        public static String API_KEY = "YOUR API KEY";
+        public static String URL = "https://api.openai.com/v1/chat/completions";
+        public static String MODEL = "YOUR MODEL";
+        public static String FIX_PROMT = "Remember that you are AI Assisstant for Sorting Visualization Group 2 of the Object-Oriented Programming in Bach Khoa University. You only answer the question relate Sorting problems. Below is the question: ";
+        public static String WAITING_TEXT = "D:\\github\\SortingDemonstrate\\src\\OOP\\Group2\\Visualizer\\graphicsElements\\button\\buttonImage\\waitingText.gif";
+        public static String AVATAR_APP = "D:\\github\\SortingDemonstrate\\src\\OOP\\Group2\\Visualizer\\graphicsElements\\button\\buttonImage\\avatarApp.png";
+        public static String AVATAR_AI = "D:\\github\\SortingDemonstrate\\src\\OOP\\Group2\\Visualizer\\graphicsElements\\button\\buttonImage\\avatarAI.png";
+       public static String HELPING_BACKGROUND_PATH = "D:\\github\\SortingDemonstrate\\src\\OOP\\Group2\\Visualizer\\graphicsElements\\background\\help.png";
+}
